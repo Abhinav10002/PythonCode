0f4fdbe25073b081c9f808e19d823acd9671fffd
Abhinav Kumar Jha <abhinavkjha300@gmail.com>
2026-03-25 01:00:03 +0530

    Recursion practice

--- DSA.py
+++ DSA.py
@@ -19,9 +19,27 @@ printNumbers(1,10)'''
 
 # Factorial of a number using Recursion
 
-def factorial(n):
+'''def factorial(n):
     if n == 0:    # base case
         return 1
     
     return n * factorial(n-1)   # recursive case
 print(factorial(10))
+
+# Recursive stack
+
+def fun(n):
+    if n == 0:
+        return 
+    
+    fun(n-1)
+    print(n,end=" ")
+fun(10)'''
+
+# Fibonacci using Recursion
+def fibonacci_number(n):
+    if n == 0 or n == 1:
+        return n
+    
+    return fibonacci_number(n-1) + fibonacci_number(n-2)
+print(fibonacci_number(34))
